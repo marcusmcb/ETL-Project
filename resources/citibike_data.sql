641610707c00e9078a31da1c1e7ec51b9b03f021
Marcus McBride <marcusmcb@gmail.com>
2019-05-20 18:18:18 -0700

    updated 2018 workbook to connect to SQL

--- resources/citibike_data.sql
+++ resources/citibike_data.sql
@@ -2,11 +2,11 @@ CREATE DATABASE citibike_db;
 
 USE citibike_db;
 
-CREATE TABLE citibike_2014_data (
+CREATE TABLE citibike_2018 (
 	id INTEGER(11) AUTO_INCREMENT NOT NULL,
     Trip_Duration INTEGER(16) NOT NULL,
     Start_Time DATE,
-    End_Time DATE,
+    Stop_Time DATE,
     Start_Station_ID VARCHAR(10),
     Start_Station_Name VARCHAR(50),
     End_Station_ID VARCHAR (10),
@@ -14,6 +14,6 @@ CREATE TABLE citibike_2014_data (
     Bike_ID INTEGER(6),
     User_Type VARCHAR(20),
     Birth_Year YEAR,
-    Gender VARCHAR(1),    
+    Gender VARCHAR(10),    
     PRIMARY KEY (id)
 )
